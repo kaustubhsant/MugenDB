ff0b8e05ce8a4964817aad4f940fcd77205ee751
Nikhil Chinthapallee <nchinth@ncsu.edu>
2015-02-22 00:39:02 -0500

    Added this function MasterSlaveConnection file

--- slave/daemon_savekeymap.py
+++
@@ -1,9 +0,0 @@
-from datetime import time
-import json
-
-def savekeymap(keylocation=None):
-    keymapfile = "KeyMap.txt"
-    while True:
-        with open(keymapfile,'w') as kf:
-            json.dump(keylocation,kf)
-        time.sleep(10)
