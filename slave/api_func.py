6cb3f045d62fdc1cc7ac6e11d8fc6eefb29c92d6
Kaustubh Sant <kaustubhsant10@gmail.com>
2015-04-09 23:22:15 -0400

    Adding deleteline function

--- slave/api_func.py
+++ slave/api_func.py
@@ -6,6 +6,26 @@ class MugenDBAPI:
     def __init__(self):
         self.readbuff = {}
         self.dbfile = "MugenDBfile.txt"
+        self.keymapfile = "KeyMap.txt"
+
+    def mark_key(self,infile,seekpoint):
+	newline = ""
+    	with open(infile,'r+') as fwr:
+	   	fwr.seek(seekpoint,0)
+        	line = fwr.readline()
+		print line
+		newline = "!" 
+		newline = line[0:2] + newline + line[3:]
+		print "marked " + newline
+   		fwr.seek(seekpoint,0)
+        	fwr.write(newline.strip())
+    
+    def deleteline(self,infile,key):
+	import fileinput
+	file = fileinput.input(infile, inplace=True)
+	for line in file:
+     	    if key not in line:
+       		print line
 
     def put(self,data,keylocation,userid):
         for key in data.keys():
@@ -13,14 +33,16 @@ class MugenDBAPI:
                 print "Error: key '{0}' already exists".format(key)
                 return -1
             else:
-                with open(self.dbfile,'ab+') as dbf:
-                    dbf.seek(0,2)
-                    offset = dbf.tell()
-                    json.dump(data,dbf)
-                    dbf.write("\n")
-                    keylocation[key] = [userid,offset]
-                    print "Success: Added '{0}'".format(data)
-                    return 0
+		with open(self.keymapfile,'ab+') as keyfin:
+                    with open(self.dbfile,'ab+') as dbf:
+                        dbf.seek(0,2)
+                        offset = dbf.tell()
+                    	json.dump(data,dbf)
+                    	dbf.write("\n")
+                    	keylocation[key] = [userid,offset]
+			keyfin.write("{0}:{1}\n".format(key,keylocation[key]))
+                    	print "Success: Added '{0}'".format(data)
+                    	return 0
 
     def get(self,key,keylocation,userid):
         if key in keylocation:
@@ -36,24 +58,7 @@ class MugenDBAPI:
         else:
             print "Error: Key '{0}' does not exists".format(key)
             return -1
-	
-    '''
-    def deleteline(self,infile,seekpoint,keylocation):
-        with open(infile,'r') as fre:
-            fre.seek(seekpoint,0)
-            fre.readline()
-            with open(infile,'r+') as fwr:
-                fwr.seek(seekpoint,0)
-                line = fre.readline()
-                while(line):
-                    offset = fwr.tell()
-                    key = json.loads(line).keys()[0]
-                    fwr.write(line)
-                    keylocation[key][1] = offset
-                    line = fre.readline()
-                fwr.truncate()
-    '''
-    
+	        
     def update(self,data,keylocation,userid):
         for key in data.keys():
             if key in keylocation:
@@ -62,18 +67,20 @@ class MugenDBAPI:
                 if userid != keylocation[key][0]:
                 return "Error: No access to key '{0}'".format(key)
                 '''
-                #seekpoint = keylocation[key][1]
-                #self.deleteline(self.dbfile,seekpoint)
-                with open(self.dbfile,'ab+') as dbf:
-                    dbf.seek(0,2)
-                    offset = dbf.tell()
-                    json.dump(data,dbf)
-                    keylocation[key][1] = offset 
-                    print "Success: Updated key '{0}' with '{1}'".format(key,data[key])
-                    return 0
-        else:
-            print "Error: Key {0} does not exists".format(key)
-            return -1
+                seekpoint = keylocation[key][1]
+                self.mark_key(self.dbfile,seekpoint)
+		with open(self.keymapfile,'ab+') as keyfin:
+	            with open(self.dbfile,'ab+') as dbf:
+                    	dbf.seek(0,2)
+                    	offset = dbf.tell()
+                    	json.dump(data,dbf)
+                    	keylocation[key][1] = offset 
+			keyfin.write("{}:{}\n".format(key,keylocation[key]))
+                    	print "Success: Updated key '{0}' with '{1}'".format(key,data[key])
+                    	return 0
+            else:
+            	print "Error: Key {0} does not exists".format(key)
+            	return -1
 
     def delete(self,key,keylocation,userid):
         if key in keylocation:
@@ -82,14 +89,16 @@ class MugenDBAPI:
             if userid != keylocation[key][0]:
                 return "Error: No access to key '{0}'".format(key)
             '''
-            #seekpoint = keylocation[key][1]
-            #self.deleteline(self.dbfile,seekpoint)
+            seekpoint = keylocation[key][1]
+            self.mark_key(self.dbfile,seekpoint)
             keylocation.pop(key,None)
+	    self.deleteline(self.keymapfile,key)
             print "Success: Deleted key '{0}'".format(key)
             return 0
         else:
             print "Error: Key '{0}' does not exists".format(key)
             return -1
+
 
 if __name__ == "__main__":
     
@@ -100,15 +109,16 @@ if __name__ == "__main__":
     keylocation = {}
     dbcaller = MugenDBAPI()
     dbcaller.put(data, keylocation, userid)
-    print keylocation
+    print "After put " + str(keylocation)
     dbcaller.put({'address':'raleigh'}, keylocation, userid)
+    print "After put " + str(keylocation)
     dbcaller.put({'address':'new york'}, keylocation, userid)
-    print keylocation
     print dbcaller.get('address', keylocation, userid)
     data = {'name':'ankit'}
     dbcaller.update(data, keylocation, userid)
     print keylocation
     print dbcaller.get('name', keylocation, userid)
-    dbcaller.delete('name',keylocation)
+    print dbcaller.get('address', keylocation, userid)
+    dbcaller.delete('name',keylocation,userid)
     print keylocation
     
