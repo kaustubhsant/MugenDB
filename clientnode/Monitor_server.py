08051da0109c0a134824441d7be2ae62599ff96b
Kaustubh Sant <kaustubhsant10@gmail.com>
2015-04-10 02:16:15 -0400

    Modified files to use json objects

--- clientnode/Monitor_server.py
+++ clientnode/Monitor_server.py
@@ -1,7 +1,7 @@
+import sys
 import socket
 import sqlite3
-
-
+import json
 
 def Server_Socket_Creation():
 	s = socket.socket()
@@ -12,28 +12,33 @@ def Server_Socket_Creation():
 	while True:
 	   c,addr = s.accept()     
 	   print 'Got connection from', addr
-	   data = c.recv(1024)
-	   user_info = data.split(",")
-	   username_input = user_info[0]
-	   password_input = user_info[1]
+	   data = json.loads(c.recv(1024))
+	   #user_info = data.split(",")
+	   #username_input = user_info[0]
+	   #password_input = user_info[1]
 	   conn_2 = sqlite3.connect('authentication_info.db')
 	   c_2 = conn_2.cursor()
-	   for row in c_2.execute("SELECT password from user_info where username = '%s'" % username_input):
+	   password = ""
+	   for row in c_2.execute("SELECT password from user_info where username = '%s'" % data['username']):
 	   	password = row
 	   conn_2.commit()
 	   conn_2.close()
-	   username_split = '(u\'' + username_input + '\',)'
-	   password_split = '(u\'' + password_input + '\',)'
-	   if str(password_split) != str(password):
-		sys.exit('Incorrect Password, terminating... \n')
-
+	   print password
+	   if not password:
+		c.send('Login failed')
+		sys.exit(1)
+	   #username_split = '(u\'' + username_input + '\',)'
+	   #password_split = '(u\'' + password_input + '\',)'
+	   if data['password'] != password[0]:
+		c.send('Login failed')
+		sys.exit(1)
 	   print 'Login Successful\n'
 
 	   c.send('Thank you for connecting')
-	   request_key_value_pair = c.recv(1024)
-	   request_key_value = request_key_value_pair.split('@')
-	   print 'Request is ' + request_key_value[0]
-	   print 'received Key_value is ' + request_key_value[1]
+	   request_key_value_pair = json.loads(c.recv(1024))
+	   #request_key_value = request_key_value_pair.split('@')
+	   print 'Request is ' + str(request_key_value_pair)
+	   #print 'received Key_value is ' + request_key_value[1]
 	   c.send("successfully received input data and request")
 	   c.close()  
 
@@ -41,11 +46,11 @@ if __name__ == "__main__":
 
 	conn = sqlite3.connect('authentication_info.db')
 	c = conn.cursor()
-	c.execute('''DROP TABLE user_info''')
+	#c.execute('''DROP TABLE user_info''')
 	c.execute("CREATE TABLE user_info (username text, password text)")
 	c.execute("INSERT INTO user_info values('shashank','goud')")
 	c.execute("INSERT INTO user_info values('ankit','bhandari')")
-	c.execute("INSERT INTO user_info values('kausthub','sant')")
+	c.execute("INSERT INTO user_info values('kaustubh','sant')")
 	c.execute("INSERT INTO user_info values('nikhil','chintapallee')")
 	conn.commit()
 	conn.close()
--- clientnode/user_client.py
+++ clientnode/user_client.py
@@ -1,29 +1,61 @@
 import sys
-import hashlib
-import getpass
+#import hashlib
+#import getpass
 import socket
+import json
 
-user_name = raw_input('Please enter username: ')
-user_password = raw_input('Please Enter Password:')
-user_details = user_name + ',' + user_password
+user_details ={}
+input_to_monitor = {}
+attempts = 0 
+username = raw_input('Please enter username: ')
+userpassword = raw_input('Please Enter Password:')
+user_details['username'] = username
+user_details['password'] = userpassword
+
 s = socket.socket()
 host = socket.gethostname();
 port = 13464
 s.connect((host, port))
-s.send(user_details)
-print s.recv(1024)
-request = raw_input('Enter your request: ')
-if str(request) =="put" :
-	user_input=raw_input("Enter key and value: ")
-if str(request)=="delete" :
-	user_input=raw_input("Enter key: ")
-if str(request)=="update" :
-	user_input=raw_input("Enter key and value: ")
-if str(request)=="get" :
-	user_input=raw_input("Enter key: ")
+s.send(json.dumps(user_details))
 
-input_to_monitor = request+ '@' + user_input
-s.send(input_to_monitor)
+while(attempts <= 3):
+	attempts = attempts + 1 
+	if s.recv(1024) == "Login failed" :
+	    username = raw_input('Please enter username: ')
+	    userpassword = raw_input('Please Enter Password:')
+	    user_details['username'] = username
+	    user_details['password'] = userpassword
+	    s.send(json.dumps(user_details))
+	else:
+	    break
+
+if attempts >3:
+    sys.exit(1)
+	
+request = raw_input('Enter your request: ')
+
+input_to_monitor['userid'] = username
+input_to_monitor['request'] = request
+
+if str(request) =="put" :
+	userinput = {}
+	data = raw_input("Enter data as key:value\n")
+	userinput[data.split(":")[0]] = data.split(":")[1]
+	input_to_monitor['data'] = userinput
+if str(request)=="get" :
+	userinput=raw_input("Enter key: ")
+	input_to_monitor['data'] = userinput
+if str(request)=="update" :
+	userinput = {}
+	data = raw_input("Enter data as key:value\n")
+	userinput[data.split(":")[0]] = data.split(":")[1]
+	input_to_monitor['data'] = userinput
+if str(request)=="delete" :
+	userinput=raw_input("Enter key: ")
+	input_to_monitor['data'] = userinput
+
+#input_to_monitor = request+ '@' + user_input
+s.send(json.dumps(input_to_monitor))
 print s.recv(1024)
 s.close          
 
