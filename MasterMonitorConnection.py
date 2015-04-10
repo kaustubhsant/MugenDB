aaf39a6edc1169a56ea56611e0da7b709a961540
Ankit Bhandari <ankit491@gmail.com>
2015-04-10 01:00:48 -0400

    Adding MasterMonitor connection

---
+++ MasterMonitorConnection.py
@@ -0,0 +1,32 @@
+import socket
+import threading
+import thread
+from multiprocessing.pool import ThreadPool
+from datetime import time
+import json	
+from api_func import MugenDBAPI
+import logging
+import time
+import traceback
+
+
+class MasterMonitorConnection:
+    def __init__(self,portNumber):
+        self.portNumber = portNumber
+	self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)         
+	self.host = socket.gethostname()               
+	self.sock.bind((self.host, self.portNumber)) 
+        self.monitors = dict()
+        with open("config/monitor.txt") as myfile:
+            for line in myfile:
+                name, endpoint = line.partition("=")[::2]
+                self.monitors[name] = endpoint
+        myfile.close()
+        self.pool = ThreadPool(10) #TODO : configure this
+
+    def listen(self):
+	print 'listening....' #TODO : log this
+	#self.pool.apply_async(savekeymap,args=(self.keylocation,))
+	while True:
+   	    request, addr = self.sock.recvfrom(1024)
+    	  #self.pool.apply_async(ServeRequest, args=(request,self.monitor,keylocation))
