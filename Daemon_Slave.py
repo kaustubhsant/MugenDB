28cc317875fffb5f8d4ebdd52fbfdc94a8db511a
Nikhil Chinthapallee <nchinth@ncsu.edu>
2015-04-10 23:25:26 -0400

    Modified time to Datetime

--- Daemon_Slave.py
+++ Daemon_Slave.py
@@ -1,8 +1,9 @@
 import socket
 import time
+import datetime
 from threading import Thread
 
-slaves = {'Slave1':time.time(),'Slave2':time.time(),'Slave3':time.time(),'Slave4':time.time()}
+slaves = {'Slave1':datetime.datetime.now(),'Slave2':datetime.datetime.now(),'Slave3':datetime.datetime.now(),'Slave4':datetime.datetime.now()}
 time_for_dead = 15  #15 seconds
 
 class DaemonSlave:
@@ -16,20 +17,20 @@ class DaemonSlave:
 		print 'listening....' #TODO : log this
 		while True:
    	  		request, addr = self.sock.recvfrom(1024)
-			slaves[request]=time.time()
+			slaves[request]=datetime.datetime.now()
 			print request
 
 	def run(self):
 		thread = Thread(target = self.listen, args = ())
 		thread.start()
 		while True:
-			time.sleep(2)
+			time.sleep(20)
 			dead = self.get_dead_slaves()
 			print 'printing dead...'
 			print str(dead)
 		
 	def get_dead_slaves(self):
-		silent = [slave for (slave, hbtime) in slaves.items() if time.time()-hbtime > time_for_dead]
+		silent = [slave for (slave, hbtime) in slaves.items() if (datetime.datetime.now()-hbtime).seconds > time_for_dead]
 		return silent
 
 
