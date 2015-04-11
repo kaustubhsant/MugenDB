30213e68191f48e347b165505a769ff290064a72
Ankit Bhandari <ankit491@gmail.com>
2015-04-11 02:56:22 -0400

    Editing and Adding hashing

--- MasterMonitorConnection.py
+++ MasterMonitorConnection.py
@@ -8,7 +8,10 @@ from api_func import MugenDBAPI
 import logging
 import time
 import traceback
+from hash_ring import MemcacheRing
 
+#data = dict{}
+masternum =1
 
 class MasterMonitorConnection:
     def __init__(self,portNumber):
@@ -28,5 +31,34 @@ class MasterMonitorConnection:
 	print 'listening....' #TODO : log this
 	#self.pool.apply_async(savekeymap,args=(self.keylocation,))
 	while True:
-   	    request, addr = self.sock.recvfrom(1024)
-    	  #self.pool.apply_async(ServeRequest, args=(request,self.monitor,keylocation))
+		  client,address = self.server.accept()
+	   	  	#request, addr = self.sock.recvfrom(1024)
+	    	  #self.pool.apply_async(ServeRequest, args=(request,self.monitor,keylocation))
+		  rec_req = json.loads(client.recv(1024))
+		  if rec_req['data'].keys():
+			key = rec_req['data'].keys()[0]
+		  else:
+			key = rec_req['data']
+		  hxmd5 = calculatemd5(key)
+		  rec_req['md5'] = hxmd5
+		  rec_req['Master'] = masternum
+		  self.server.close()
+		#connect to slave and send rec_req
+		  
+                  s = socket.socket()
+		  host = socket.gethostname();
+	       	  port = 10000
+		  s.connect((host, port))
+		  s.send(json.dumps(rec_req))
+		  
+                  if json.loads(s.recv(1024))==rec_req
+		     s.close()
+		  else
+		     s.send(json.dumps(rec_req))
+
+
+def calculatemd5(key)
+    	memcache_servers = ['localhost:10000']
+	ring = HashRing(memcache_servers)
+	server = ring.get_node('key')
+	
