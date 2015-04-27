48d1eb6f1691d3c0ac747b63da15734dc1d0a651
Kaustubh Sant <kaustubhsant10@gmail.com>
2015-04-27 00:16:47 -0400

    Adding replication to other nodes

--- slavenode/daemon_repl.py
+++ slavenode/daemon_repl.py
@@ -7,13 +7,19 @@ class Backup_Data:
 		self.dbfile = "MugenDBfile.txt"
 		self.backupfile = "Backup_MugenDBfile.txt"
 		self.backuppatch = "Backup_MugenDBfile.patch"
+		self.replpass = list()
+		with open("config/password.txt",'r') as fin:
+			for line in fin:
+				self.replpass.append(line.strip().split("="))
 
 	def backup(self):		
 		if(os.path.isfile(self.backupfile) == False):
 			f = file(self.backupfile,"w")
-		os.system("diff -u {} {} > {}".format(self.backupfile,self.dbfile,self.backuppatch))
-		os.system("patch {} {}".format(self.backupfile,self.backuppatch))
-		#os.system("scp Backup_MugenDBfile.patch username@backupnode:/home/backup")
+		os.system("diff -u {0} {1} > {2}".format(self.backupfile,self.dbfile,self.backuppatch))
+		os.system("patch {0} {1}".format(self.backupfile,self.backuppatch))
+		os.system("sshpass -p '{0}' scp Backup_MugenDBfile.patch backup@{1}:/home/backup".format(self.replpass[0][1],self.replpass[0][0]))
+		os.system("sshpass -p '{0}' scp Backup_MugenDBfile.patch backup@{1}:/home/backup".format(self.replpass[1][1],self.replpass[1][0]))
+		os.system("sshpass -p '{0}' scp Backup_MugenDBfile.patch backup@{1}:/home/backup".format(self.replpass[2][1],self.replpass[2][0]))
 		return 0
 		
 
