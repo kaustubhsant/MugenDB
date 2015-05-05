39cf5d8c9d6e28431fab9db32f19dc527d35a935
Kaustubh Sant <kaustubhsant10@gmail.com>
2015-05-05 15:06:42 -0400

    Updating daemon_repl

--- slavenode/daemon_repl.py
+++ slavenode/daemon_repl.py
@@ -24,21 +24,24 @@ class Backup_Data:
 			f = file(self.backupfile,"w")
 		os.system("diff -u {0} {1} > {2}".format(self.backupfile,self.dbfile,self.backuppatch))
 		os.system("patch {0} {1}".format(self.backupfile,self.backuppatch))
-		t1 = threading.Thread(target=self.repl,args=(self.replpass[0][1],self.replpass[0][0],))
-		t2 = threading.Thread(target=self.repl,args=(self.replpass[1][1],self.replpass[1][0],))
-		t3 = threading.Thread(target=self.repl,args=(self.replpass[2][1],self.replpass[2][0],))
+		t1 = threading.Thread(target=self.repl,args=(self.replpass[0],))
+		t2 = threading.Thread(target=self.repl,args=(self.replpass[1],))
+		t3 = threading.Thread(target=self.repl,args=(self.replpass[2],))
 		t1.start()
 		t2.start()
 		t3.start()
 		t1.join()
 		t2.join()
 		t3.join()
+		os.system("rm {}".format(self.backuppatch))
 		return 0
 	
-	def repl(self,passwd,node):
-		logger.debug("replicating on {}".format(node))
-		os.system("sshpass -p '{0}' scp Backup_MugenDBfile.patch backup_slave@{1}:/home/backup".format(passwd,node))
-		logger.debug("replicated on {}".format(node))
+	def repl(self,repl):
+		logger.debug("replicating on {0}@{1}".format(repl[0],repl[1]))
+		os.system("cp Backup_MugenDBfile.patch Backup_MugenDBfile_{}.patch".format(repl[0]))
+		os.system("sshpass -p '{0}' scp Backup_MugenDBfile_{1}.patch backup_slave@{2}:/home/backup_slave/backup".format(repl[2],repl[0],repl[1]))
+		os.system("rm Backup_MugenDBfile_{}.patch".format(repl[0]))
+		logger.debug("replicated on {0}@{1}".format(repl[0],repl[1]))
 		
 
 if __name__ == "__main__":
